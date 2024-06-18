32b33c130a8d7d5e233cfa632f4f7de755abdb6f
jnorm23 <joshua_norman11@hotmail.com>
2024-06-18 19:13:44 -0400

    Node modules and answer to Question 1

--- problems/01-remove-last-vowel.js
+++ problems/01-remove-last-vowel.js
@@ -6,6 +6,18 @@ Vowels are the letters "a", "e", "i", "o", "u".
 
 // Your code here 
 
+
+function removeLastVowel(word) {
+    const vowels = 'aeiouAEIOU';                                   // Assign vowels within the function
+    let arr = word.split('');                                      // Convert the string to an array to seperate the individual letters
+    for (let i = arr.length - 1; i >= 0; i--) {                    // Loop thru the entire array
+        if (vowels.includes(arr[i])) {                             // Remove and break the loop if the character is a vowel
+            arr.splice(i, 1);
+            break;
+        }
+    }
+        return arr.join('');                                       // Return the array joined together back into a string
+}
 // console.log(removeLastVowel('bootcamp')); // 'bootcmp'
 // console.log(removeLastVowel('better')); // 'bettr'
 // console.log(removeLastVowel('graph')); // 'grph'
